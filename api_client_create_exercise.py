46beefd550af8fa179c6c5c4bf3d450955f0a603
Максим Качмазов <maxim.kachmazov@bnovo.ru>
2026-05-13 09:40:54 +0300

    7.2 Применяем Pydantic в API клиентах

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -1,33 +1,37 @@
-from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
-from clients.exercises.exercises_client import get_exercises_client, CreateExerciseRequestDict
-from clients.files.files_client import get_files_client, CreateFileRequestDict
-from clients.private_http_builder import AuthenticationUserDict
-from clients.users.public_users_client import get_public_users_client, CreateUserRequestDict
+from clients.courses.courses_client import get_courses_client
+from clients.courses.courses_schema import CreateCourseRequestSchema
+from clients.exercises.exercises_client import get_exercises_client
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema
+from clients.files.files_client import get_files_client
+from clients.files.files_schema import CreateFileRequestSchema
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema
 from tools.fakers import get_random_email
 
 public_users_client = get_public_users_client()
 
-# Создаем пользователя
-create_user_request = CreateUserRequestDict(
+# Вместо CreateUserRequestDict используем CreateUserRequestSchema
+create_user_request = CreateUserRequestSchema(
     email=get_random_email(),
     password="string",
-    lastName="string",
-    firstName="string",
-    middleName="string"
+    last_name="string",   # Передаем аргументы в формате snake_case вместо camelCase
+    first_name="string",  # Передаем аргументы в формате snake_case вместо camelCase
+    middle_name="string"  # Передаем аргументы в формате snake_case вместо camelCase
 )
 create_user_response = public_users_client.create_user(create_user_request)
 
-# Инициализируем клиенты
-authentication_user = AuthenticationUserDict(
-    email=create_user_request['email'],
-    password=create_user_request['password']
+# Используем атрибуты вместо ключей
+authentication_user = AuthenticationUserSchema(
+    email=create_user_request.email,
+    password=create_user_request.password
 )
 files_client = get_files_client(authentication_user)
 courses_client = get_courses_client(authentication_user)
 exercises_client = get_exercises_client(authentication_user)
 
-# Загружаем файл
-create_file_request = CreateFileRequestDict(
+# Вместо CreateFileRequestDict используем CreateFileRequestSchema
+create_file_request = CreateFileRequestSchema(
     filename="image.png",
     directory="courses",
     upload_file="./testdata/files/image.png"
@@ -35,28 +39,28 @@ create_file_request = CreateFileRequestDict(
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
-# Создаем курс
-create_course_request = CreateCourseRequestDict(
+# Вместо CreateCourseRequestDict используем CreateCourseRequestSchema
+create_course_request = CreateCourseRequestSchema(
     title="Python",
-    maxScore=100,
-    minScore=10,
+    max_score=100,   # Передаем аргументы в формате snake_case вместо camelCase
+    min_score=10,    # Передаем аргументы в формате snake_case вместо camelCase
     description="Python API course",
-    estimatedTime="2 weeks",
-    previewFileId=create_file_response['file']['id'],
-    createdByUserId=create_user_response['user']['id']
+    estimated_time="2 weeks",              # Передаем аргументы в формате snake_case вместо camelCase
+    preview_file_id=create_file_response.file.id,    # Используем атрибуты вместо ключей
+    created_by_user_id=create_user_response.user.id  # Используем атрибуты вместо ключей
 )
 create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
 
-# Создаем задание
-create_exercise_request = CreateExerciseRequestDict(
+# Вместо CreateExerciseRequestDict используем CreateExerciseRequestSchema
+create_exercise_request = CreateExerciseRequestSchema(
     title="Exercise 1",
-    courseId=create_course_response['course']['id'],
-    maxScore=5,
-    minScore=1,
+    course_id=create_course_response.course.id,  # Используем атрибуты вместо ключей; snake_case вместо camelCase
+    max_score=5,    # Передаем аргументы в формате snake_case вместо camelCase
+    min_score=1,    # Передаем аргументы в формате snake_case вместо camelCase
     description="Exercise 1",
-    estimatedTime="5 minutes",
-    orderIndex=0
+    estimated_time="5 minutes",  # Передаем аргументы в формате snake_case вместо camelCase
+    order_index=0                # Передаем аргументы в формате snake_case вместо camelCase
 )
 create_exercise_response = exercises_client.create_exercise(create_exercise_request)
 print('Create exercise data:', create_exercise_response)
--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -1,83 +1,16 @@
-from typing import TypedDict
-
 from httpx import Response
 
 from clients.api_client import APIClient
-from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
-
-
-class Exercise(TypedDict):
-    """
-    Описание структуры задания.
-    """
-    id: str
-    title: str
-    courseId: str
-    maxScore: int
-    minScore: int
-    orderIndex: int
-    description: str
-    estimatedTime: str
-
-
-class GetExercisesResponseDict(TypedDict):
-    """
-    Описание структуры ответа получения списка заданий.
-    """
-    exercises: list[Exercise]
-
-
-class GetExerciseResponseDict(TypedDict):
-    """
-    Описание структуры ответа получения задания.
-    """
-    exercise: Exercise
-
-
-class CreateExerciseResponseDict(TypedDict):
-    """
-    Описание структуры ответа создания задания.
-    """
-    exercise: Exercise
-
-
-class UpdateExerciseResponseDict(TypedDict):
-    """
-    Описание структуры ответа обновления задания.
-    """
-    exercise: Exercise
-
-
-class GetExercisesQueryDict(TypedDict):
-    """
-    Описание структуры запроса на получение списка заданий.
-    """
-    courseId: str
-
-
-class CreateExerciseRequestDict(TypedDict):
-    """
-    Описание структуры запроса на создание задания.
-    """
-    title: str
-    courseId: str
-    maxScore: int
-    minScore: int
-    description: str
-    estimatedTime: str
-    orderIndex: int | None
-
-
-class UpdateExerciseRequestDict(TypedDict):
-    """
-    Описание структуры запроса на обновление задания.
-    """
-    title: str | None
-    maxScore: int | None
-    minScore: int | None
-    description: str | None
-    estimatedTime: str | None
-    orderIndex: int | None
+from clients.exercises.exercises_schema import (
+    GetExercisesQuerySchema,
+    GetExercisesResponseSchema,
+    GetExerciseResponseSchema,
+    CreateExerciseRequestSchema,
+    CreateExerciseResponseSchema,
+    UpdateExerciseRequestSchema,
+    UpdateExerciseResponseSchema,
+)
+from clients.private_http_builder import get_private_http_client, AuthenticationUserSchema
 
 
 class ExercisesClient(APIClient):
@@ -85,14 +18,14 @@ class ExercisesClient(APIClient):
     Клиент для работы с /api/v1/exercises
     """
 
-    def get_exercises_api(self, query: GetExercisesQueryDict) -> Response:
+    def get_exercises_api(self, query: GetExercisesQuerySchema) -> Response:
         """
         Метод получения списка заданий для определенного курса.
 
-        :param query: Словарь с courseId.
+        :param query: Объект с course_id.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get("/api/v1/exercises", params=query)
+        return self.get("/api/v1/exercises", params=query.model_dump(by_alias=True))
 
     def get_exercise_api(self, exercise_id: str) -> Response:
         """
@@ -103,24 +36,25 @@ class ExercisesClient(APIClient):
         """
         return self.get(f"/api/v1/exercises/{exercise_id}")
 
-    def create_exercise_api(self, request: CreateExerciseRequestDict) -> Response:
+    def create_exercise_api(self, request: CreateExerciseRequestSchema) -> Response:
         """
         Метод создания задания.
 
-        :param request: Словарь с параметрами для создания задания (title, courseId, maxScore, minScore, description, estimatedTime, orderIndex).
+        :param request: Объект с title, course_id, max_score, min_score, description,
+        estimated_time, order_index.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.post("/api/v1/exercises", json=request)
+        return self.post("/api/v1/exercises", json=request.model_dump(by_alias=True))
 
-    def update_exercise_api(self, exercise_id: str, request: UpdateExerciseRequestDict) -> Response:
+    def update_exercise_api(self, exercise_id: str, request: UpdateExerciseRequestSchema) -> Response:
         """
         Метод обновления данных задания.
 
         :param exercise_id: Идентификатор задания.
-        :param request: Словарь с параметрами для обновления задания.
+        :param request: Объект с параметрами для обновления задания.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.patch(f"/api/v1/exercises/{exercise_id}", json=request)
+        return self.patch(f"/api/v1/exercises/{exercise_id}", json=request.model_dump(by_alias=True))
 
     def delete_exercise_api(self, exercise_id: str) -> Response:
         """
@@ -131,53 +65,53 @@ class ExercisesClient(APIClient):
         """
         return self.delete(f"/api/v1/exercises/{exercise_id}")
 
-    def get_exercises(self, query: GetExercisesQueryDict) -> GetExercisesResponseDict:
+    def get_exercises(self, query: GetExercisesQuerySchema) -> GetExercisesResponseSchema:
         """
         Метод получения списка заданий.
-        
-        :param query: Словарь с courseId.
-        :return: Словарь со списком заданий.
+
+        :param query: Объект с course_id.
+        :return: Объект со списком заданий.
         """
         response = self.get_exercises_api(query)
-        return response.json()
+        return GetExercisesResponseSchema.model_validate_json(response.text)
 
-    def get_exercise(self, exercise_id: str) -> GetExerciseResponseDict:
+    def get_exercise(self, exercise_id: str) -> GetExerciseResponseSchema:
         """
         Метод получения задания.
-        
+
         :param exercise_id: Идентификатор задания.
-        :return: Словарь с данными задания.
+        :return: Объект с данными задания.
         """
         response = self.get_exercise_api(exercise_id)
-        return response.json()
+        return GetExerciseResponseSchema.model_validate_json(response.text)
 
-    def create_exercise(self, request: CreateExerciseRequestDict) -> CreateExerciseResponseDict:
+    def create_exercise(self, request: CreateExerciseRequestSchema) -> CreateExerciseResponseSchema:
         """
         Метод создания задания.
-        
-        :param request: Словарь с параметрами для создания задания.
-        :return: Словарь с данными созданного задания.
+
+        :param request: Объект с параметрами для создания задания.
+        :return: Объект с данными созданного задания.
         """
         response = self.create_exercise_api(request)
-        return response.json()
+        return CreateExerciseResponseSchema.model_validate_json(response.text)
 
-    def update_exercise(self, exercise_id: str, request: UpdateExerciseRequestDict) -> UpdateExerciseResponseDict:
+    def update_exercise(self, exercise_id: str, request: UpdateExerciseRequestSchema) -> UpdateExerciseResponseSchema:
         """
         Метод обновления задания.
-        
+
         :param exercise_id: Идентификатор задания.
-        :param request: Словарь с параметрами для обновления задания.
-        :return: Словарь с данными обновленного задания.
+        :param request: Объект с параметрами для обновления задания.
+        :return: Объект с данными обновленного задания.
         """
         response = self.update_exercise_api(exercise_id, request)
-        return response.json()
+        return UpdateExerciseResponseSchema.model_validate_json(response.text)
 
 
-def get_exercises_client(user: AuthenticationUserDict) -> ExercisesClient:
+def get_exercises_client(user: AuthenticationUserSchema) -> ExercisesClient:
     """
     Функция создаёт экземпляр ExercisesClient с уже настроенным HTTP-клиентом.
 
-    :param user: Словарь с данными пользователя.
+    :param user: Объект с данными пользователя.
     :return: Готовый к использованию ExercisesClient.
     """
     return ExercisesClient(client=get_private_http_client(user))
