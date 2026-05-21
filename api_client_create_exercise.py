304fac91b8dbe4feac1c5159855efd33c0614448
Максим Качмазов <maxim.kachmazov@bnovo.ru>
2026-05-21 12:18:42 +0300

    7.4 Знакомство с Faker. Практика

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -7,21 +7,13 @@ from clients.files.files_schema import CreateFileRequestSchema
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
-from tools.fakers import fake
 
 public_users_client = get_public_users_client()
 
-# Вместо CreateUserRequestDict используем CreateUserRequestSchema
-create_user_request = CreateUserRequestSchema(
-    email=fake.email(),
-    password="string",
-    last_name="string",   # Передаем аргументы в формате snake_case вместо camelCase
-    first_name="string",  # Передаем аргументы в формате snake_case вместо camelCase
-    middle_name="string"  # Передаем аргументы в формате snake_case вместо camelCase
-)
+# Больше нет необходимости передавать значения, они будут генерировать автоматически
+create_user_request = CreateUserRequestSchema()
 create_user_response = public_users_client.create_user(create_user_request)
 
-# Используем атрибуты вместо ключей
 authentication_user = AuthenticationUserSchema(
     email=create_user_request.email,
     password=create_user_request.password
@@ -30,37 +22,22 @@ files_client = get_files_client(authentication_user)
 courses_client = get_courses_client(authentication_user)
 exercises_client = get_exercises_client(authentication_user)
 
-# Вместо CreateFileRequestDict используем CreateFileRequestSchema
-create_file_request = CreateFileRequestSchema(
-    filename="image.png",
-    directory="courses",
-    upload_file="./testdata/files/image.png"
-)
+# Автоматическая генерация данных, кроме необходимых параметров
+create_file_request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
-# Вместо CreateCourseRequestDict используем CreateCourseRequestSchema
+# Используем сгенерированные значения для создания курса
 create_course_request = CreateCourseRequestSchema(
-    title="Python",
-    max_score=100,   # Передаем аргументы в формате snake_case вместо camelCase
-    min_score=10,    # Передаем аргументы в формате snake_case вместо camelCase
-    description="Python API course",
-    estimated_time="2 weeks",              # Передаем аргументы в формате snake_case вместо camelCase
-    preview_file_id=create_file_response.file.id,    # Используем атрибуты вместо ключей
-    created_by_user_id=create_user_response.user.id  # Используем атрибуты вместо ключей
+    preview_file_id=create_file_response.file.id,
+    created_by_user_id=create_user_response.user.id
 )
 create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
 
-# Вместо CreateExerciseRequestDict используем CreateExerciseRequestSchema
+# Используем сгенерированные значения для создания задания
 create_exercise_request = CreateExerciseRequestSchema(
-    title="Exercise 1",
-    course_id=create_course_response.course.id,  # Используем атрибуты вместо ключей; snake_case вместо camelCase
-    max_score=5,    # Передаем аргументы в формате snake_case вместо camelCase
-    min_score=1,    # Передаем аргументы в формате snake_case вместо camelCase
-    description="Exercise 1",
-    estimated_time="5 minutes",  # Передаем аргументы в формате snake_case вместо camelCase
-    order_index=0                # Передаем аргументы в формате snake_case вместо camelCase
+    course_id=create_course_response.course.id
 )
 create_exercise_response = exercises_client.create_exercise(create_exercise_request)
 print('Create exercise data:', create_exercise_response)
--- clients/exercises/exercises_schema.py
+++ clients/exercises/exercises_schema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field, ConfigDict
+from tools.fakers import fake
 
 
 class ExerciseSchema(BaseModel):
@@ -46,13 +47,13 @@ class CreateExerciseRequestSchema(BaseModel):
     """
     model_config = ConfigDict(populate_by_name=True)
 
-    title: str
-    course_id: str = Field(alias="courseId")
-    max_score: int = Field(alias="maxScore")
-    min_score: int = Field(alias="minScore")
-    description: str
-    estimated_time: str = Field(alias="estimatedTime")
-    order_index: int | None = Field(default=None, alias="orderIndex")
+    title: str = Field(default_factory=fake.sentence)
+    course_id: str = Field(alias="courseId", default_factory=fake.uuid4)
+    max_score: int = Field(alias="maxScore", default_factory=fake.max_score)
+    min_score: int = Field(alias="minScore", default_factory=fake.min_score)
+    description: str = Field(default_factory=fake.text)
+    estimated_time: str = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+    order_index: int | None = Field(alias="orderIndex", default_factory=fake.integer)
 
 
 class CreateExerciseResponseSchema(BaseModel):
@@ -68,12 +69,12 @@ class UpdateExerciseRequestSchema(BaseModel):
     """
     model_config = ConfigDict(populate_by_name=True)
 
-    title: str | None = None
-    max_score: int | None = Field(default=None, alias="maxScore")
-    min_score: int | None = Field(default=None, alias="minScore")
-    description: str | None = None
-    estimated_time: str | None = Field(default=None, alias="estimatedTime")
-    order_index: int | None = Field(default=None, alias="orderIndex")
+    title: str | None = Field(default_factory=fake.sentence)
+    max_score: int | None = Field(alias="maxScore", default_factory=fake.max_score)
+    min_score: int | None = Field(alias="minScore", default_factory=fake.min_score)
+    description: str | None = Field(default_factory=fake.text)
+    estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+    order_index: int | None = Field(alias="orderIndex", default_factory=fake.integer)
 
 
 class UpdateExerciseResponseSchema(BaseModel):
