a67e0c54b5eb5c755d0957fa98120e784c9c5375
Максим Качмазов <maxim.kachmazov@bnovo.ru>
2026-05-27 17:52:05 +0300

    9.1 Рефакторинг и подготовка к написанию API автотестов

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -5,7 +5,7 @@ import pytest
 from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
-from fixtures.users import UserFixture  # Заменяем импорт
+from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
@@ -15,13 +15,11 @@ from tools.fakers import fake
 @pytest.mark.users
 @pytest.mark.regression
 @pytest.mark.parametrize("email", ["mail.ru", "gmail.com", "example.com"])
-def test_create_user(email: str, public_users_client: PublicUsersClient):  # Используем фикстуру API клиента
-    # Удалили инициализацию API клиента из теста
+def test_create_user(email: str, public_users_client: PublicUsersClient):
     request = CreateUserRequestSchema(email=fake.email(domain=email))
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
-    assert request.email.endswith(f"@{email}")
     assert_status_code(response.status_code, HTTPStatus.OK)
     assert_create_user_response(request, response_data)
 
@@ -30,10 +28,7 @@ def test_create_user(email: str, public_users_client: PublicUsersClient):  # И�
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_get_user_me(
-        private_users_client: PrivateUsersClient,
-        function_user: UserFixture,
-):
+def test_ger_user_me(function_user: UserFixture, private_users_client: PrivateUsersClient):
     response = private_users_client.get_user_me_api()
     response_data = GetUserResponseSchema.model_validate_json(response.text)
 
