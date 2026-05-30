149181f502940513dc4b556abdae59dcabbc7216
Максим Качмазов <maxim.kachmazov@bnovo.ru>
2026-05-30 13:54:44 +0300

    9.4 Практикуемся в написании API автотестов. Часть 3. Практика 2/5

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -6,10 +6,15 @@ from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import (
     CreateExerciseRequestSchema,
     CreateExerciseResponseSchema,
+    GetExerciseResponseSchema,
 )
 from fixtures.courses import CourseFixture
+from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercises import assert_create_exercise_response
+from tools.assertions.exercises import (
+    assert_create_exercise_response,
+    assert_get_exercise_response,
+)
 from tools.assertions.schema import validate_json_schema
 
 
@@ -39,5 +44,26 @@ class TestExercises:
 
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_create_exercise_response(request, response_data)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_exercise(
+            self,
+            exercises_client: ExercisesClient,
+            function_exercise: ExerciseFixture
+    ):
+        """
+        Проверяет получение задания через GET /api/v1/exercises/{exercise_id}.
+
+        Убеждается, что:
+        - статус-код ответа равен 200 OK;
+        - данные задания в ответе совпадают с ранее созданным заданием из function_exercise;
+        - тело ответа соответствует JSON-схеме GetExerciseResponseSchema.
+        """
+        response = exercises_client.get_exercise_api(function_exercise.response.exercise.id)
+        response_data = GetExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_get_exercise_response(response_data, function_exercise.response)
 
         validate_json_schema(response.json(), response_data.model_json_schema())
