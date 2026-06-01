0337191d32419785aface9ca27f77098d7bf9a04
Максим Качмазов <maxim.kachmazov@bnovo.ru>
2026-06-01 09:30:59 +0300

    10.2 Интеграция Allure в автотесты. Практика

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -36,9 +36,12 @@ from tools.assertions.schema import validate_json_schema
 @allure.tag(AllureTag.EXERCISES, AllureTag.REGRESSION)
 @allure.epic(AllureEpic.LMS)  # Добавили epic
 @allure.feature(AllureFeature.EXERCISES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.EXERCISES)
 class TestExercises:
     @allure.tag(AllureTag.CREATE_ENTITY)
     @allure.story(AllureStory.CREATE_ENTITY)
+    @allure.sub_suite(AllureStory.CREATE_ENTITY)
     @allure.title("Create exercise")
     @allure.severity(Severity.BLOCKER)
     def test_create_exercise(
@@ -69,6 +72,7 @@ class TestExercises:
 
     @allure.tag(AllureTag.GET_ENTITY)
     @allure.story(AllureStory.GET_ENTITY)
+    @allure.sub_suite(AllureStory.GET_ENTITY)
     @allure.title("Get exercise")
     def test_get_exercise(
             self,
@@ -94,6 +98,7 @@ class TestExercises:
     @allure.tag(AllureTag.DELETE_ENTITY)
     @allure.tag(AllureTag.UPDATE_ENTITY)
     @allure.story(AllureStory.UPDATE_ENTITY)
+    @allure.sub_suite(AllureStory.UPDATE_ENTITY)
     @allure.title("Update exercise")
     @allure.severity(Severity.CRITICAL)
     def test_update_exercise(
@@ -122,6 +127,7 @@ class TestExercises:
 
     @allure.tag(AllureTag.DELETE_ENTITY)
     @allure.story(AllureStory.DELETE_ENTITY)
+    @allure.sub_suite(AllureStory.DELETE_ENTITY)
     @allure.title("Delete exercise")
     @allure.severity(Severity.CRITICAL)
     def test_delete_exercise(
@@ -153,6 +159,7 @@ class TestExercises:
 
     @allure.tag(AllureTag.GET_ENTITIES)
     @allure.story(AllureStory.GET_ENTITIES)
+    @allure.sub_suite(AllureStory.GET_ENTITIES)
     @allure.title("Get exercises")
     @allure.severity(Severity.BLOCKER)
     def test_get_exercises(
