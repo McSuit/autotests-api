08455e694d4efa650969438afb9e3a5586671733
Максим Качмазов <maxim.kachmazov@bnovo.ru>
2026-07-02 16:52:11 +0300

    11.5 Улучшение работы с API клиентами с помощью Enum для URI

--- clients/courses/courses_client.py
+++ clients/courses/courses_client.py
@@ -2,13 +2,10 @@ import allure
 from httpx import Response
 
 from clients.api_client import APIClient
-from clients.courses.courses_schema import (
-    GetCoursesQuerySchema,
-    CreateCourseRequestSchema,
-    CreateCourseResponseSchema,
-    UpdateCourseRequestSchema,
-)
-from clients.private_http_builder import get_private_http_client, AuthenticationUserSchema
+from clients.courses.courses_schema import GetCoursesQuerySchema, CreateCourseRequestSchema, UpdateCourseRequestSchema, \
+    CreateCourseResponseSchema
+from clients.private_http_builder import AuthenticationUserSchema, get_private_http_client
+from tools.routes import APIRoutes  # Импортируем enum APIRoutes
 
 
 class CoursesClient(APIClient):
@@ -21,10 +18,10 @@ class CoursesClient(APIClient):
         """
         Метод получения списка курсов.
 
-        :param query: Объект с user_id.
+        :param query: Словарь с userId.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get("/api/v1/courses", params=query.model_dump(by_alias=True))
+        return self.get(APIRoutes.COURSES, params=query.model_dump(by_alias=True))
 
     @allure.step("Get course by id {course_id}")
     def get_course_api(self, course_id: str) -> Response:
@@ -34,18 +31,18 @@ class CoursesClient(APIClient):
         :param course_id: Идентификатор курса.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get(f"/api/v1/courses/{course_id}")
+        return self.get(f"{APIRoutes.COURSES}/{course_id}")
 
     @allure.step("Create course")
     def create_course_api(self, request: CreateCourseRequestSchema) -> Response:
         """
         Метод создания курса.
 
-        :param request: Объект с title, max_score, min_score, description, estimated_time,
-        preview_file_id, created_by_user_id.
+        :param request: Словарь с title, maxScore, minScore, description, estimatedTime,
+        previewFileId, createdByUserId.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.post("/api/v1/courses", json=request.model_dump(by_alias=True))
+        return self.post(APIRoutes.COURSES, json=request.model_dump(by_alias=True))
 
     @allure.step("Update course by id {course_id}")
     def update_course_api(self, course_id: str, request: UpdateCourseRequestSchema) -> Response:
@@ -53,10 +50,13 @@ class CoursesClient(APIClient):
         Метод обновления курса.
 
         :param course_id: Идентификатор курса.
-        :param request: Объект с title, max_score, min_score, description, estimated_time.
+        :param request: Словарь с title, maxScore, minScore, description, estimatedTime.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.patch(f"/api/v1/courses/{course_id}", json=request.model_dump(by_alias=True))
+        return self.patch(
+            f"{APIRoutes.COURSES}/{course_id}",
+            json=request.model_dump(by_alias=True)
+        )
 
     @allure.step("Delete course by id {course_id}")
     def delete_course_api(self, course_id: str) -> Response:
@@ -66,7 +66,7 @@ class CoursesClient(APIClient):
         :param course_id: Идентификатор курса.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.delete(f"/api/v1/courses/{course_id}")
+        return self.delete(f"{APIRoutes.COURSES}/{course_id}")
 
     def create_course(self, request: CreateCourseRequestSchema) -> CreateCourseResponseSchema:
         response = self.create_course_api(request)
--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -4,6 +4,7 @@ from httpx import Response
 from clients.api_client import APIClient
 from clients.public_http_builder import get_public_http_client
 from clients.users.users_schema import CreateUserResponseSchema, CreateUserRequestSchema
+from tools.routes import APIRoutes  # Импортируем enum APIRoutes
 
 
 class PublicUsersClient(APIClient):
@@ -11,6 +12,7 @@ class PublicUsersClient(APIClient):
     Клиент для работы с /api/v1/users
     """
 
+    @allure.step("Create user")
     def create_user_api(self, request: CreateUserRequestSchema) -> Response:
         """
         Метод создает пользователя.
@@ -18,7 +20,7 @@ class PublicUsersClient(APIClient):
         :param request: Словарь с email, password, lastName, firstName, middleName.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.post("/api/v1/users", json=request.model_dump(by_alias=True))
+        return self.post(APIRoutes.USERS, json=request.model_dump(by_alias=True))
 
     @allure.step("Create user")
     def create_user(self, request: CreateUserRequestSchema) -> CreateUserResponseSchema:
